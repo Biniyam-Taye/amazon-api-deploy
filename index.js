9e917c44d203a6d09e88127378916605659b4a68
Biniyam Taye <biniyamtaye04@gmail.com>
2025-10-16 22:47:29 +0300

    initial comit for amazon clon e backend deployment

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { total } = req.body; // amount in smallest currency unit (cents)
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total, // e.g. $50 = 5000
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
